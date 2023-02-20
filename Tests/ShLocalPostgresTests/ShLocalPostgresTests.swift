770805e706c1c39ef99b230aaab6f1eda71c695d
Full Queue Developer <FullQueueDeveloper@FullQueueDeveloper.com>
2023-02-19 23:27:14 -0500

    postgres

--- Tests/ShLocalPostgresTests/ShLocalPostgresTests.swift
+++ Tests/ShLocalPostgresTests/ShLocalPostgresTests.swift
@@ -3,35 +3,35 @@ import Foundation
 import XCTest
 
 final class ShLocalPostgresTests: XCTestCase {
-  
+
   let config = LocalPostgres(
-    superuser: "root",
-    superuserPassword: "root",
+    superuser: "postgres",
+    superuserPassword: "postgres",
     role: "tomato",
     password: "fresh_password",
     databaseStem: "supermarket",
     databaseTails: ["development", "testing"])
-  
+
   func testCreateAndDestroy() throws {
-    
+
     let beforeStatus = try ShLocalPostgres.currentStatus()
     XCTAssertFalse(beforeStatus.contains("tomato"))
-    
+
     try config.createAll()
-    
+
     let afterCreateStatus = try ShLocalPostgres.currentStatus()
     XCTAssertTrue(afterCreateStatus.contains("tomato"))
-    
+
     XCTAssertNoThrow(try ShLocalPostgres.ensureDBConnection(
       name: "supermarket_development",
       owner: "tomato",
       password: "fresh_password"))
-    
+
     try config.destroyAll()
-    
+
     let afterDestroyStatus = try ShLocalPostgres.currentStatus()
     XCTAssertFalse(afterDestroyStatus.contains("tomato"))
-    
+
     XCTAssertThrowsError(try ShLocalPostgres.ensureDBConnection(
       name: "supermarket_development",
       owner: "tomato",
