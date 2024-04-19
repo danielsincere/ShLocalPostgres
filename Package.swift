4bf8e23df7900f14789d573f88f58b19b7ffe810
Your Name <FullQueueDeveloper@FullQueueDeveloper.com>
2024-04-18 22:32:40 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.8
 import PackageDescription
 
 let package = Package(name: "ShLocalPostgres",
-  platforms: [.macOS(.v12)],
+  platforms: [.macOS(.v13)],
   products: [
     .library(name: "ShLocalPostgres", targets: ["ShLocalPostgres"]),
 
   ],
   dependencies: [
-    .package(url: "https://github.com/FullQueueDeveloper/Sh", from: "1.2.0"),
+    .package(url: "https://github.com/DanielSincere/Sh", from: "1.3.0"),
   ],
   targets: [
     .target(
